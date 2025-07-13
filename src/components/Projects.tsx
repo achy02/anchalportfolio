be6086b79bfef01c818b65b943c2bd9ee27c2769
Anchal Tiwari <109622868+achy02@users.noreply.github.com>
2025-07-13 14:13:08 +0530

    Update Projects.tsx

--- src/components/Projects.tsx
+++ src/components/Projects.tsx
@@ -163,7 +163,7 @@ export const Projects = () => {
             className="bg-gradient-primary hover:bg-gradient-secondary border-0 text-white hover:scale-105 transition-all duration-300"
             asChild
           >
-            <a href="https://github.com/anchal-tiwari" target="_blank" rel="noopener noreferrer">
+            <a href="https://github.com/achy02" target="_blank" rel="noopener noreferrer">
               <Github className="w-5 h-5 mr-2" />
               View All Projects on GitHub
             </a>
